97650a36f8e64f0cfa40864e72dde2e567cb6797
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-11-13 04:25:11 -0300

    Checando se existe um usuário pelo id passado nos parâmetros de requisição

--- src/index.js
+++ src/index.js
@@ -47,7 +47,16 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  if(!validate(id)){
+    return response.status(400).json({error:"Invalid ID!"});
+  }
+  const user = users.find(user=>user.id===id);
+  if(!user){
+    return response.status(404).json({error:"User not found!"})
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
